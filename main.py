e6259059affec517649c73134971df1137f42903
DABer1805 <DABer1805@yandex.ru>
2024-04-27 12:37:38 +0300

    feat: complite Task №2

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import pygame
 import requests
 from io import BytesIO
 
-# Задаем координаты и масштаб карты
+# Задаем координаты, масштаб и предельные значения карты
 latitude = 55.75396
 longitude = 37.620393
 zoom = 10
+MIN_ZOOM = 1
+MAX_ZOOM = 19
 
-# Формируем URL запроса к статической карте Yandex
-url = f"https://static-maps.yandex.ru/1.x/?ll=" \
-      f"{longitude},{latitude}&z={zoom}&l=map"
+# Формируем URL запрос к MapsAPI
+url = f"https://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}" \
+      f"&z={zoom}&l=map"
 
 response = requests.get(url)
 # Грузим нашу картинку
@@ -18,7 +20,7 @@ map_image = pygame.image.load(BytesIO(response.content))
 # Создаем окно Pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Большая задача по Maps API. Часть №1")
+pygame.display.set_caption("Большая задача по Maps API. Часть №2")
 
 # Отображаем карту в окне
 screen.blit(map_image, (0, 0))
@@ -30,5 +32,32 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                # Увеличиваем масштаб
+                if zoom < MAX_ZOOM:
+                    zoom += 1
+                    # Формируем новый URL запрос
+                    url = f"https://static-maps.yandex.ru/1.x/" \
+                          f"?ll={longitude},{latitude}&z={zoom}&l=map"
+                response = requests.get(url)
+                # Грузим обновленную картинку
+                map_image = pygame.image.load(BytesIO(response.content))
+                # Отображаем обновленную карту в окне
+                screen.blit(map_image, (0, 0))
+                pygame.display.flip()
+            elif event.key == pygame.K_PAGEDOWN:
+                # Уменьшаем масштаб
+                if zoom > MIN_ZOOM:
+                    zoom -= 1
+                    # Формируем новый URL запрос
+                    url = f"https://static-maps.yandex.ru/1.x/" \
+                          f"?ll={longitude},{latitude}&z={zoom}&l=map"
+                response = requests.get(url)
+                # Грузим обновленную картинку
+                map_image = pygame.image.load(BytesIO(response.content))
+                # Отображаем обновленную карту в окне
+                screen.blit(map_image, (0, 0))
+                pygame.display.flip()
 
 pygame.quit()
